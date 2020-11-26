bb8f3d0a0d6a42778314fdddfbc4df8b5bd642f1
obrm <obrm770@gmail.com>
2020-11-26 16:01:31 +0200

    More refactoring- functions replace redundant code

--- script.js
+++ script.js
@@ -1,12 +1,29 @@
 'use strict';
 
-let secretNumber = Math.trunc(Math.random() * 20) + 1;
-let score = 20;
-let highScore = 0;
-
 const displayMessage = message => {
   document.querySelector('.message').textContent = message;
 };
+
+const createRandom = () => Math.trunc(Math.random() * 20) + 1;
+
+const styleBody = color => (document.body.style.backgroundColor = color);
+
+const changeNumberElement = (width, textContent) => {
+  document.querySelector('.number').style.width = width;
+  document.querySelector('.number').textContent = textContent;
+};
+
+const setScore = num => {
+  document.querySelector('.score').textContent = num;
+};
+
+const changeCheckButton = bool => {
+  document.querySelector('.check').disabled = bool;
+};
+
+let secretNumber = createRandom();
+let score = 20;
+let highScore = 0;
 
 const messageColor = color => {
   document.querySelector('.message').style.color = color;
@@ -14,9 +31,9 @@ const messageColor = color => {
 
 // document.querySelector('.number').textContent = secretNumber;
 
-// document.querySelector('.guess').addEventListener('focus', function () {
-//   console.log(secretNumber);
-// });
+document.querySelector('.guess').addEventListener('focus', function () {
+  console.log(secretNumber);
+});
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
@@ -26,11 +43,10 @@ document.querySelector('.check').addEventListener('click', function () {
       displayMessage('😖 Input must be a number between 1 and 20!!!');
     } else if (guess === secretNumber) {
       messageColor('#eee');
-      document.body.style.backgroundColor = '#60b347';
-      document.querySelector('.number').style.width = '30rem';
-      document.querySelector('.number').textContent = secretNumber;
+      styleBody('#60b347');
+      changeNumberElement('30rem', secretNumber);
       displayMessage('🎉 Correct number!!!');
-      document.querySelector('.check').disabled = true;
+      changeCheckButton(true);
       if (highScore < score) {
         document.querySelector('.highscore').textContent = score;
         highScore = score;
@@ -41,26 +57,25 @@ document.querySelector('.check').addEventListener('click', function () {
         ? displayMessage('📉 Too low!!!')
         : displayMessage('📈 Too High!!!');
       score--;
-      document.querySelector('.score').textContent = score;
+      setScore(score);
     }
   } else {
     messageColor('#eee');
     score = 0;
-    document.querySelector('.score').textContent = score;
-    document.body.style.backgroundColor = 'red';
+    setScore(score);
+    styleBody('red');
     displayMessage('💥 You lost the game!');
   }
 });
 
 document.querySelector('.again').addEventListener('click', function () {
-  document.querySelector('.check').disabled = false;
+  changeCheckButton(false);
   messageColor('#eee');
-  document.querySelector('.number').style.width = '15rem';
+  changeNumberElement('15rem', '?');
   document.querySelector('.guess').value = '';
-  document.querySelector('.number').textContent = '?';
   displayMessage('Start guessing...');
-  document.body.style.backgroundColor = '#222';
-  document.querySelector('.score').textContent = 20;
+  styleBody('#222');
+  setScore(20);
   score = 20;
-  secretNumber = Math.trunc(Math.random() * 20) + 1;
+  secretNumber = createRandom();
 });
